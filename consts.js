ff36b6acc3a13a0011a9b2b5259fd9cb9324ad22
nvinayvarma189 <vinay.n.varma189@gmail.com>
2022-01-19 23:21:26 +0530

    remove consts.js

--- consts.js
+++
@@ -1,6 +0,0 @@
-let REDIRECTION_ENABLED = true;
-let HIDE_FEED_ENABLED = false;
-const REDIRECT_URL = "https://google.com/";
-const BLOCK_URL = "https://www.reddit.com/";
-
-module.exports = {REDIRECTION_ENABLED, HIDE_FEED_ENABLED, REDIRECT_URL, BLOCK_URL};
